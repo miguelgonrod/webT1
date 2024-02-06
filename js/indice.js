9b0741299712d7e9fd474083147efce610354d61
miguelgonrod <miguelgonrod2004@gmail.com>
2024-02-06 17:40:58 -0500

    added more css

---
+++ js/indice.js
@@ -0,0 +1,9 @@
+<script>
+        let color = document.getElementById("color").innerHTML;
+        if (color == "Azul") {
+          document.body.style.backgroundColor = "#0000FF"
+        }
+        if (color == "Rojo") {
+          document.body.style.backgroundColor = "#FF0000"
+        }
+</script>
