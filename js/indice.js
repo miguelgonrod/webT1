9e4275993ccf04f9504c754cc3548ebfca38c7e2
nauJ120 <juan.duarte017@hotmail.com>
2024-02-06 23:10:50 -0500

    Addition of js
    Tried to use the hover on mouse to change the color (not successful), also, added the correct path of the js

--- js/indice.js
+++ js/indice.js
@@ -1,9 +1,30 @@
-<script>
-        let color = document.getElementById("color").innerHTML;
-        if (color == "Azul") {
-          document.body.style.backgroundColor = "#0000FF"
-        }
-        if (color == "Rojo") {
-          document.body.style.backgroundColor = "#FF0000"
-        }
-</script>
+/*document.getElementById("myID").addEventListener('mouseenter', function() {
+    document.getElementById("myID").style.backgroundColor = "green"});
+        
+document.getElementById("myID").addEventListener('mouseleave', function() {
+    document.getElementById("myID").style.backgroundColor = "red"});
+*/
+
+/*document.addEventListener('DOMContentLoaded', function () {
+    let sections = document.querySelectorAll('.hover-section');
+
+    sections.forEach(function (section) {
+        section.addEventListener('mouseenter', function () {
+            section.style.backgroundColor = '#e0e0e0';
+        });
+
+        section.addEventListener('mouseleave', function () {
+            section.style.backgroundColor = '';
+        });
+    });
+});
+*/
+var color = [, "#3C9EE7", "#E7993C",
+            "#E73C99", "#3CE746", "#E7993C"];
+ 
+document.querySelector("section").addEventListener(
+    "mouseover", function () {
+ 
+        document.querySelector("section").style.background
+            = color[(Math.floor(Math.random() * color.length))];
+    })
