96371d9ae5cb15d70fcb05126542f2df5c21cf38
Juan Diego Pérez Duarte <juan.duarte017@hotmail.com>
2024-02-08 10:15:40 -0500

    Mouse hover
    Still not working the mouse hover, but now is receiving the functions

--- js/indice.js
+++ js/indice.js
@@ -1,30 +1,12 @@
-/*document.getElementById("myID").addEventListener('mouseenter', function() {
-    document.getElementById("myID").style.backgroundColor = "green"});
-        
-document.getElementById("myID").addEventListener('mouseleave', function() {
-    document.getElementById("myID").style.backgroundColor = "red"});
-*/
+function selectArea(){
+    var id = document.getElementById('section1');
+    console.log("este id: ", id)
+    document.getElementById('section1').style.backgroundColor = 'blue'
+}
 
-/*document.addEventListener('DOMContentLoaded', function () {
-    let sections = document.querySelectorAll('.hover-section');
 
-    sections.forEach(function (section) {
-        section.addEventListener('mouseenter', function () {
-            section.style.backgroundColor = '#e0e0e0';
-        });
-
-        section.addEventListener('mouseleave', function () {
-            section.style.backgroundColor = '';
-        });
-    });
-});
-*/
-var color = [, "#3C9EE7", "#E7993C",
-            "#E73C99", "#3CE746", "#E7993C"];
- 
-document.querySelector("section").addEventListener(
-    "mouseover", function () {
- 
-        document.querySelector("section").style.background
-            = color[(Math.floor(Math.random() * color.length))];
-    })
+function leaveArea(){
+    var id = document.getElementById('section1');
+    console.log("este id: ", id)
+    document.getElementById('section1').style.backgroundColor = 'red'
+}
